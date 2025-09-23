f14868b57fc2f7ec28b07b72e56e31453d976c78
Vetsou <virees@posteo.net>
2025-09-23 12:47:09 +0200

    Vite config url fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
   resolve: {
     alias: {
-      '$builder': resolve(__dirname, 'src/lib/builder'),
-      '$helper': resolve(__dirname, 'src/lib/builder/helper'),
-      '$components': resolve(__dirname, 'src/lib/components')
+      '$builder': fileURLToPath(new URL('src/lib/builder', import.meta.url)),
+      '$helper': fileURLToPath(new URL('src/lib/builder/helper', import.meta.url)),
+      '$components': fileURLToPath(new URL('src/lib/components', import.meta.url))
     }
   },
   test: {
