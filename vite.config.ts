ade604597fb29b9973fa35ad61a59f0ef1d9809b
Vetsou <virees@posteo.net>
2025-09-23 13:13:08 +0200

    Added types to tsconfig file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
