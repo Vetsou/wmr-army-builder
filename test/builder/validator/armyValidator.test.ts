29f7c658e08c3b51962eeff1bc2141efd6fb6610
Vetsou <virees@posteo.net>
2025-10-05 21:57:23 +0200

    Add unit validator tests

--- test/builder/validator/armyValidator.test.ts
+++ test/builder/validator/armyValidator.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import type { IBuilderState } from '$builder/store'
 import { get, type Writable } from 'svelte/store'
 import { ArmyErrors, formatError } from '$builder/validator/errorMessages'
