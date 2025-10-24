eb1b03c84c2d83d5b72c3c9da50c3c8a6091e409
Vetsou <virees@posteo.net>
2025-10-24 02:05:20 +0200

    Added initial RoR

---
+++ src/lib/builder/helper/regimentsHelper.ts
@@ -0,0 +1,94 @@
+import type { IBuilderState } from '$builder/store'
+
+type CountAsRuleResult = {
+  units: [string, ISchemaUnit][]
+  upgrades: [string, ISchemaUpgrade][]
+}
+
+const filterByType = (
+  state: IBuilderState,
+  result: CountAsRuleResult,
+  type: CountAsRuleType
+): CountAsRuleResult => {
+  if (type === 'any') return { units: result.units, upgrades: [] }
+
+  const hasOr = type.includes('|')
+  if (hasOr) {
+    const unitTypes = type.split('|').map(t => t.trim())
+    return {
+      units: result.units.filter(([_, u]) => unitTypes.includes(u.type)),
+      upgrades: []
+    }
+  }
+
+  const hasAnd = type.includes('&')
+  if (hasAnd) {
+    const [unitType, upgradeType] = type
+      .split('&')
+      .map(t => t.trim()) as [UnitType, UpgradeType]
+
+    return {
+      units: result.units.filter(([_, u]) => unitType.includes(u.type)),
+      upgrades: Object.entries(state.lookup.armyUpgrades ?? {}).filter(([_, upg]) => upgradeType.includes(upg.type))
+    }
+  }
+
+  return {
+    units: result.units.filter(([_, u]) => u.type === type as UnitType),
+    upgrades: []
+  }
+}
+
+const filterUnitsByTags = (
+  units: [string, ISchemaUnit][],
+  rule: RegimentCountAsRule
+): [string, ISchemaUnit][] => {
+  const highestCost = rule.costType === 'highest'
+    ? Math.max(...units.map(([_, u]) => u.points))
+    : null
+
+  return units.filter(([_, u]) => {
+    // Flying
+    if (rule.tags?.mustBeRanged && u.range === undefined) return false
+    if (rule.tags?.requiredSize && u.size !== rule.tags.requiredSize) return false
+    if (rule.costType === 'highest' && u.points !== highestCost) return false
+    return true
+  })
+}
+
+const filterUpgradesByTags = (
+  upgrades: [string, ISchemaUpgrade][],
+  rule: RegimentCountAsRule
+) => {
+  return upgrades.filter(([_, _upg]) => {
+    // Flying
+    if (rule.tags?.mustCauseTerror /* ADD UNIT TERROR CHECK HERE */) return false
+    return true
+  })
+}
+
+export const getRegimentCountAsRuleUnits = (
+  state: IBuilderState,
+  regiment: IArmyRegiment
+): CountAsRuleResult => {
+  if (!regiment.countAsRules) return { units: [], upgrades: [] }
+
+  const countAsRule = regiment.countAsRules[state.armyName] ?? regiment.countAsRules.any
+
+  let result: CountAsRuleResult = {
+    units: Object.entries(state.units).filter(([_, u]) => u.max || u.armyMax),
+    upgrades: []
+  }
+
+  // Rule should always have type or unitName
+  if (countAsRule.tags?.unitName) {
+    result.units = result.units.filter(([name, _]) => name === countAsRule.tags?.unitName)
+  } else if (countAsRule.type !== 'any') {
+    result = filterByType(state, result, countAsRule.type!)
+  }
+
+  return {
+    units: filterUnitsByTags(result.units, countAsRule),
+    upgrades: filterUpgradesByTags(result.upgrades, countAsRule)
+  }
+}
--- src/lib/types/regiments.d.ts
+++ src/lib/types/regiments.d.ts
@@ -1,10 +1,39 @@
 declare global {
   /**
+   * Count as rule UnitType string
+   */
+  type CountAsRuleType = UnitType
+    | `${UnitType}|${UnitType}`
+    | `${UnitType}&${UpgradeType}`
+    | 'any'
+
+  /**
+   * Count as rule tags
+   */
+  type CountAsRuleTag = {
+    unitName?: string
+    mustFly?: boolean
+    mustCauseTerror?: boolean
+    mustBeRanged?: boolean
+    requiredSize?: number
+  }
+
+  /**
+   * Determines how regiment is counted within the army
+   */
+  type RegimentCountAsRule = {
+    type?: CountAsRuleType
+    tags?: CountAsRuleTag
+    costType: 'highest' | 'any'
+  }
+
+  /**
    * Regiment of Renown inside schema
    */
   interface ISchemaRegiment extends ISchemaUnit {
     incompatibleWith?: string[]
     incompatibleFactions?: string[]
+    countAsRules?: Record<string, RegimentCountAsRule>
   }
 
   /**
