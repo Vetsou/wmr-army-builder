676c5cfa89c04fbc9e1f4409f1e3a7f2adfb157e
Vetsou <virees@posteo.net>
2025-11-23 17:52:01 +0100

    Update

--- src/lib/components/url/UrlPanel.svelte
+++ src/lib/components/url/UrlPanel.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { encodeArmyToUrl } from '$builder/serialization'
+  import builderStore from '$builder/store'
 
+
+  let shareUrl = ""
+
+  onMount(() => {
+    const encoded = encodeArmyToUrl(builderStore.getState())
+    shareUrl = `${window.location.origin}/factions/orcs?${encoded}`
+  })
 </script>
 
-<div>
-  Panel
+<div class="p-4 space-y-3">
+  <div class="mt-2 p-2 bg-gray-200 rounded break-all">
+    <strong>Your URL:</strong>
+    {shareUrl}
+  </div>
 </div>
